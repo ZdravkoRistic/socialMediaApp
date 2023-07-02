9be456ca44a7141bb5cf93b075e16be7ba2d9adc
ZdravkoRistic <easycodemaster@gmail.com>
2023-07-02 11:11:46 +0200

    Changed url for deploymend frontend

--- frontend/src/AppLayout.js
+++ frontend/src/AppLayout.js
@@ -11,8 +11,9 @@ import { restoreUser } from './store/userSlice';
 if (process.env.NODE_ENV === 'development') {
 	axios.defaults.baseURL = 'http://localhost:4000/api';
 } else {
-	axios.defaults.baseURL = 'https://selectit-social.vercel.app/api';
+	axios.defaults.baseURL = 'https://vercel.com/zdravkoristic/social-media-app';
 }
+	
 
 axios.interceptors.request.use((config) => {
 	if (localStorage.hasOwnProperty('sm_token')) {
